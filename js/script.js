786873178e0532b978a445d8d9a24b3df01d80a4
nasim <admin@xeba.co.nz>
2016-02-11 00:13:33 +1300

    .

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-$(document).foundation();
+$(function() {
+    var BV = new $.BigVideo();
+    BV.init();
+    BV.show('video/alertt.mp4',{ambient:true});
+
+    $('#next-btn').on('click', function(e) {
+        $('#big-video-wrap').addClass('animated slideOutUp');
+        $('#next-btn').css('display','none');
+    });
+
+    setInterval(function(){
+        $('#next-btn').addClass('animated bounce').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            $(this).removeClass('animated bounce');
+        });
+    },5000);
+
+});
